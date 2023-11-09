c677d12f114530a6069427fc1e11d3b9c4f4fd2b
samiksha12n <samikshaunesarkar@gmail.com>
2023-11-09 14:28:38 +0530

    Varaibles and Data Types

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   //Declare variable name with string, int, double and display them
+            string name = "Sam";
+            int age = 23;
+            double height = 152;
+            Console.WriteLine(" My Name is:\t"+name+"\n My Age is:\t"+age+"\n Height :\t"+height);
+            Console.ReadKey();
+
+
+        }
+    }
+}
